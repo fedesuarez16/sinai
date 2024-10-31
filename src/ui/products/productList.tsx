2c1554492d4d26819715232fbeba79ee2d0c3b56
fedesuarez16 <fedesuarez16@gmail.com>
2024-10-31 01:35:22 -0300

    feat: idk

--- src/ui/products/productList.tsx
+++ src/ui/products/productList.tsx
@@ -42,9 +42,7 @@ export const ProductList = async ({ products }: { products: Commerce.MappedProdu
 													})}
 												</p>
 											)}
-											<p className="mt-1 text-[13px] text-green-700">
-												Precio pagando con transferencia/efectivo
-											</p>
+											<p className="mt-1 text-[13px] text-green-700">Hasta 6 cuotas sin interes</p>
 										</footer>
 									</div>
 								</article>
